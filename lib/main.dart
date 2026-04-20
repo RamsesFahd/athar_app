9794d4332c187a1b3dfb314f804832f2329123a9
Rimas Alharthi <121484836+RamsesFahd@users.noreply.github.com>
2026-04-20 22:48:31 +0300

    fixed OTP issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:athar_app/features/cultural_archive/logic/cultural_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -20,7 +22,11 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-
+  if (kDebugMode) {
+    await FirebaseAuth.instance.setSettings(
+      appVerificationDisabledForTesting: true,
+    );
+  }
 
   runApp(
     const ProviderScope(
